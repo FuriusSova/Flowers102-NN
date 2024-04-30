7a043014ed9a5a7871fa8ed22a25506af628a6b8
FuriusSova <dsova2004@gmail.com>
2024-04-30 02:52:32 +0100

    Good train loss but increasing validation loss

--- FlowerNN.py
+++ FlowerNN.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 transform = transforms.Compose([
     transforms.Resize((128, 128)),
 	transforms.RandomHorizontalFlip(),
-	transforms.RandomRotation(30),
+	transforms.RandomRotation(15),
     transforms.ToTensor(), 
     transforms.Normalize(
         mean=[0.4330, 0.3819, 0.2964],
@@ -29,21 +29,22 @@ train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
 val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
 
-conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3)
-conv2 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3)
-conv3 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3)
-conv4 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3)
+conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3)
+conv2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3)
+# conv3 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3)
+# conv4 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3)
 pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
 pool2 = nn.MaxPool2d(kernel_size=4, stride=4)
 
 # Uncomment to figure out the in_features number for the first Linear layer
 # for i, (images, labels) in enumerate(train_loader):
 # 	x = conv1(images)
-# 	x = conv2(x)
 # 	x = pool1(x)
-# 	x = conv3(x)
-# 	x = conv4(x)
+# 	x = conv2(x)
 # 	x = pool2(x)
+# 	# x = conv3(x)
+# 	# x = conv4(x)
+# 	# x = pool2(x)
 # 	print(x.shape)
 # 	break
 
@@ -52,21 +53,21 @@ class FlowerNN(nn.Module):
 		super().__init__()
 		self.conv1 = conv1
 		self.conv2 = conv2
-		self.conv3 = conv3
-		self.conv4 = conv4
+		# self.conv3 = conv3
+		# self.conv4 = conv4
 		self.pool1 = pool1
 		self.pool2 = pool2
-		self.fc1 = nn.Linear(in_features=32 * 14 * 14, out_features=128)
+		self.fc1 = nn.Linear(in_features=32 * 15 * 15, out_features=128)
 		self.fc2 = nn.Linear(128, 64)
 		self.fc3 = nn.Linear(64, 102)
 
 		self.activation_fn = activation_fn
 
 	def forward(self, x):
-		x = self.activation_fn(self.conv1(x))
-		x = self.pool1(self.activation_fn(self.conv2(x)))
-		x = self.activation_fn(self.conv3(x))
-		x = self.pool2(self.activation_fn(self.conv4(x)))
+		x = self.pool1(self.activation_fn(self.conv1(x)))
+		x = self.pool2(self.activation_fn(self.conv2(x)))
+		# x = self.activation_fn(self.conv3(x))
+		# x = self.pool2(self.activation_fn(self.conv4(x)))
 		x = torch.flatten(x, 1)
 		x = self.activation_fn(self.fc1(x))
 		x = self.activation_fn(self.fc2(x))
