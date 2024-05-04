26b7174a1281a7225a44a33d92b56e4ceaecd1e4
FuriusSova <dsova2004@gmail.com>
2024-05-04 16:11:57 +0100

    Best perfomance so far

--- FlowerNN.py
+++ FlowerNN.py
@@ -13,8 +13,6 @@ transform = v2.Compose([
     v2.ToImage(),
     v2.Resize((224, 224)),
 	v2.ToDtype(torch.float32, scale=True),
-	v2.RandomHorizontalFlip(),
-	v2.RandomRotation(15),
     v2.Normalize(
         mean=[0.4330, 0.3819, 0.2964],
         std=[0.2587, 0.2093, 0.2210]
@@ -25,28 +23,27 @@ train_set = datasets.Flowers102(root='./train', split='train', download=True, tr
 val_set = datasets.Flowers102(root='./valid', split='val', download=True, transform=transform)
 test_set = datasets.Flowers102(root='./test', split='test', download=True, transform=transform)
 
-batch_size = round(len(train_set) / 12)
+batch_size = round(len(train_set) / 8)
 
 train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
 val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False)
 test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
 
 conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3)
-conv2 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3)
-# conv3 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3)
-# conv4 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3)
+conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3)
+conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3)
+conv4 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3)
 pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
 pool2 = nn.MaxPool2d(kernel_size=4, stride=4)
 
 # Uncomment to figure out the in_features number for the first Linear layer
 # for i, (images, labels) in enumerate(train_loader):
 # 	x = conv1(images)
-# 	x = pool1(x)
 # 	x = conv2(x)
+# 	x = pool1(x)
+# 	x = conv3(x)
+# 	x = conv4(x)
 # 	x = pool2(x)
-# 	# x = conv3(x)
-# 	# x = conv4(x)
-# 	# x = pool2(x)
 # 	print(x.shape)
 # 	break
 
@@ -57,27 +54,28 @@ class FlowerNN(nn.Module):
 		super().__init__()
 		self.conv1 = conv1
 		self.conv2 = conv2
-		# self.conv3 = conv3
-		# self.conv4 = conv4
+		self.conv3 = conv3
+		self.conv4 = conv4
 		self.pool1 = pool1
 		self.pool2 = pool2
-		self.dropout = nn.Dropout(p=0.3)
-		self.fc1 = nn.Linear(in_features=16 * 27 * 27, out_features=128)
-		# self.fc2 = nn.Linear(128, 64)
-		self.fc3 = nn.Linear(128, 102)
+		# self.dropout = nn.Dropout(p=0.3)
+		self.fc1 = nn.Linear(in_features=64 * 26 * 26, out_features=2048)
+		self.fc2 = nn.Linear(2048, 1024)
+		# self.fc3 = nn.Linear(520, 340)
+		self.fc4 = nn.Linear(1024, 102)
 
 		self.activation_fn = activation_fn
 
 	def forward(self, x):
-		x = self.pool1(self.activation_fn(self.conv1(x)))
-		x = self.pool2(self.activation_fn(self.conv2(x)))
-		# x = self.activation_fn(self.conv3(x))
-		# x = self.pool2(self.activation_fn(self.conv4(x)))
+		x = self.activation_fn(self.conv1(x))
+		x = self.pool1(self.activation_fn(self.conv2(x)))
+		x = self.activation_fn(self.conv3(x))
+		x = self.pool2(self.activation_fn(self.conv4(x)))
 		x = torch.flatten(x, 1)
 		x = self.activation_fn(self.fc1(x))
-		# x = self.activation_fn(self.fc2(x))
-		x = self.dropout(x)
-		x = self.fc3(x)
+		x = self.activation_fn(self.fc2(x))
+		# x = self.activation_fn(self.fc3(x))
+		x = self.fc4(x)
 		return x
 	
 @torch.no_grad()
@@ -102,7 +100,7 @@ def validate():
 	
 classifier = FlowerNN().to(device)
 lossFn = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(classifier.parameters(), lr=0.001, weight_decay=0.01)
+optimizer = torch.optim.SGD(classifier.parameters(), lr=0.01, momentum=0.9)
 epochs = 40
 
 # train_losses = []
