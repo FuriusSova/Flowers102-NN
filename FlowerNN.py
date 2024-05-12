942054363ef9141f9fa145f62d9dc69885df5784
FuriusSova <dsova2004@gmail.com>
2024-05-12 18:47:53 +0100

    58% val accuracy, 1.6 val loss

--- FlowerNN.py
+++ FlowerNN.py
@@ -31,24 +31,22 @@ val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False)
 test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
 
 conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=5, stride=1, padding=2)
-conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=1)
-conv3 = nn.Conv2d(in_channels=128, out_channels=512, kernel_size=3, stride=2)
-# conv4 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3)
+conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=5, stride=2, padding=2)
+conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1)
+conv4 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1)
 pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
 pool2 = nn.MaxPool2d(kernel_size=4, stride=4)
 
 # Uncomment to figure out the in_features number for the first Linear layer
-# for i, (images, labels) in enumerate(train_loader):
-# 	x = conv1(images)
-# 	x = pool1(x)
-# 	x = conv2(x)
-# 	x = pool1(x)
-# 	x = conv3(x)
-# 	x = pool1(x)
-# 	# x = conv4(x)
-# 	# x = pool2(x)
-# 	print(x.shape)
-# 	break
+for i, (images, labels) in enumerate(train_loader):
+	x = conv1(images)
+	x = conv2(x)
+	x = pool2(x)
+	x = conv3(x)
+	x = conv4(x)
+	x = pool2(x)
+	print(x.shape)
+	break
 
 torch.cuda.set_device(0)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -60,28 +58,27 @@ class FlowerNN(nn.Module):
 		self.conv1 = conv1
 		self.conv2 = conv2
 		self.conv3 = conv3
-		# self.conv4 = conv4
+		self.conv4 = conv4
 		self.pool1 = pool1
 		self.pool2 = pool2
 		self.dropout1 = nn.Dropout(p=0.2)
 		self.dropout2 = nn.Dropout(p=0.3)
 		self.dropout3 = nn.Dropout(p=0.4)
-		# self.dropout4 = nn.Dropout(p=0.4)
 		self.bn1 = nn.BatchNorm2d(128)
-		self.bn2 = nn.BatchNorm2d(512)
-		self.fc1 = nn.Linear(in_features=512 * 6 * 6, out_features=840)
-		self.fc2 = nn.Linear(840, 526)
-		self.fc3 = nn.Linear(526, 102)
+		self.bn2 = nn.BatchNorm2d(256)
+		self.bn3 = nn.BatchNorm2d(512)
+		self.fc1 = nn.Linear(in_features=512 * 7 * 7, out_features=1024)
+		self.fc2 = nn.Linear(1024, 512)
+		self.fc3 = nn.Linear(512, 102)
 
 		self.activation_fn = activation_fn
 
 	def forward(self, x):
-		x = self.pool1(self.activation_fn(self.conv1(x)))
-		x = self.bn1(self.pool1(self.activation_fn(self.conv2(x))))
-		x = self.bn2(self.pool1(self.activation_fn(self.conv3(x))))
+		x = self.activation_fn(self.conv1(x))
+		x = self.bn1(self.pool2(self.activation_fn(self.conv2(x))))
+		x = self.bn2(self.activation_fn(self.conv3(x)))
+		x = self.bn3(self.pool2(self.activation_fn(self.conv4(x))))
 		x = torch.flatten(x, 1)
-		# x = self.activation_fn(self.fc1(x))
-		# x = self.activation_fn(self.fc2(x))
 		x = self.activation_fn(self.fc1(x))
 		x = self.dropout3(x)
 		x = self.activation_fn(self.fc2(x))
